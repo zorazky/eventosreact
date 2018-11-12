9cffab026597643375bd3474d6ec740aeee873b4
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-12 07:40:35 -0600

    request to api working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import Header from './componentes/Header';
 
 
 class App extends Component {
+  token = 'SNRY4GEGTFN426E3YESE';
+
+  state = {
+    categorias: []
+  }
+  componentDidMount() {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale-es_ES`;
+
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categorias => {
+        this.setState({
+          categorias: categorias.categories
+        })
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
